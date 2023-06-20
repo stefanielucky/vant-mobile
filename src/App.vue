3c58ceb656bf751b5784fd60428d55db574cde10
zyx <3306818069@qq.com>
2023-06-20 18:19:51 +0800

    引入报错

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
+    <SliderDelete></SliderDelete>
     <Button type="primary">主要按钮</Button>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SliderDelete from './view/SliderDelete'
 import { Button } from 'vant'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    SliderDelete,
     Button
   }
 }
--- src/view/SliderDelete.vue
+++ src/view/SliderDelete.vue
@@ -1,10 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <SwipeCell>
+      <template #left>
+        <van-button square type="primary" text="选择" />
+      </template>
+      <van-cell :border="false" title="单元格" value="内容" />
+      <template #right>
+        <van-button square type="danger" text="删除" />
+        <van-button square type="primary" text="收藏" />
+      </template>
+    </SwipeCell>
+  </div>
 </template>
 
 <script>
+import { SwipeCell } from 'vant';
 export default{
   name: "SliderDelete",
+  components: {
+    SwipeCell
+  }
 }
 </script>
 
